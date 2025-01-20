924a3f537cc6d0d186a1d3658d4e39b2a00ed045
Ayush Kumar <145244924+AyushCs25@users.noreply.github.com>
2025-01-20 18:09:51 +0000

    Your commit message

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14452
+let accountEmail="ayushcs22109@gmail.com"
+var accountPassword="@K123"
+accountCity="Bhopal"   // Not preffered
+let accountState    // return Undefined
+
+// accountId = 23 // not allowed
+
+accountEmail="ak4979063@gmail.com"
+accountPassword="212121"
+accountCity="Indore"
+
+/* 
+Prefer not to use var 
+because of issue in block scope and function scope
+*/
+console.log(accountId)
+console.table([accountId,accountEmail, accountPassword, accountCity, accountState])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";   // treat all JS in newer version 
+
+//  alert(3+3)  // we are using nodejs, not browser
+
+console.log(3
+    +
+    3)  // code readability should be high
+
+
+
+let name="Sahil"
+let age=18
+let isLoggedIn = false
+
+// number = 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined yet
+// symbol => uniqueness
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
