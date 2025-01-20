0a13cfc1aca8531e71ab6ffb1563560bd18a8098
Ayush Kumar <145244924+AyushCs25@users.noreply.github.com>
2025-01-20 18:42:22 +0000

    Conversion of Datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score="12"
+
+// console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber))
+
+// let marks="332abcd"
+// let valueOfMarks= Number(marks)
+// console.log(valueOfMarks)
+// console.log(typeof (valueOfMarks))
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "Ayush" => true
+
+let someNo=33
+let stringNumber=String(someNo)
+console.log(typeof stringNumber)
