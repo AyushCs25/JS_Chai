0c3e1851c2bd4a5240b4f82f840be7408aa7d099
Ayush Kumar <145244924+AyushCs25@users.noreply.github.com>
2025-01-21 19:37:51 +0000

    String in JS

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,21 @@
+const name="Ayush Choudhary"
+const lcQues=88
+
+// console.log(name+" "+lcQues)
+console.log(`Hello, my name is ${name} and total questions I have done on my Leetcode account is ${lcQues}. `)
+//  A proffesional way to display concatenated output with all variables attached to it.
+
+const gameName=new String('mingle-game')         // Creates a String object that has all letters as key-value pairs.
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName);
+
+// console.log(gameName.charAt(2));
+
+console.log(gameName.lastIndexOf('m'));
+
+
+
