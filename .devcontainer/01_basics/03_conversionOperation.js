3f8a9582451a9bc8040aae68744aaf8d42731b5f
Ayush Kumar <145244924+AyushCs25@users.noreply.github.com>
2025-01-21 17:24:39 +0000

    Comaprison of data type

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,25 +1,53 @@
-let score="12"
+// let score="12"
 
-// console.log(typeof score)
-let valueInNumber = Number(score)
-console.log(typeof (valueInNumber))
+// // console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber))
 
-// let marks="332abcd"
-// let valueOfMarks= Number(marks)
-// console.log(valueOfMarks)
-// console.log(typeof (valueOfMarks))
+let marks="332abcd"
+let valueOfMarks= Number(marks)
+console.log(valueOfMarks)
+console.log(typeof (valueOfMarks))
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1; false => 0
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1; false => 0
 
-let isLoggedIn=1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn=1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-// 1 => true; 0 => false
-// "Ayush" => true
+// // 1 => true; 0 => false
+// // "Ayush" => true
+// "" => 0
 
-let someNo=33
-let stringNumber=String(someNo)
-console.log(typeof stringNumber)
+// let someNo=33
+// let stringNumber=String(someNo)
+// console.log(typeof stringNumber)
+
+// **************** Operations **************** //
+let value=3
+let negValue= -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+
+// let str1="Hello"
+// let str2="Hatim"
+
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2+2)
+// console.log(2+2+"1")
+
+console.log(+true)
+console.log(+"")
+
+let gameCounter=100
+let y=gameCounter++
+console.log(y)
+console.log(gameCounter)
---
+++ .devcontainer/01_basics/04_commparison.js
@@ -0,0 +1,27 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+
+console.log("2">1)
+console.log("02">1)
+
+// Here JS allows this comparison as it automatically coonverts the string data type to the other type by which it is comparing
+// Typescript is a language in which this type of comparison is not allowed. This is more a strict type language.
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)      //It gives a true value. The reason is that an equality check == and comparisons > < >= <= work differently.
+                            // Comparisons convert null to a number, treating it as 0. That's why null>=0 is true and null>0 is false.
+
+// console.log(undefined == 0)
+// console.log(undefined > 0);
+// console.log(undefined < 0);     // All three are false
+
+// Strict check ===   -> Also checks data type here
+console.log("2" === 2);
+console.log("02" > 2);
+console.log
+
+
+
