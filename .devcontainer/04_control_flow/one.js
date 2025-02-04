05a8b6a090db0bbb2233b8150595a936c09d51a9
Ayush Kumar <145244924+AyushCs25@users.noreply.github.com>
2025-02-04 17:33:29 +0530

    Basic Control Flow

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,20 @@
+// if
+
+// if (2 == "2"){
+//     console.log("executed");
+// }
+// if (2 === "2"){         // Strict Equal
+//     console.log("not executed");
+// }
+
+const userLoggedIn = false;
+const debitCard = true;
+const loggedInFromEmail = true;
+
+// if (userLoggedIn && debitCard){
+//     console.log("Allow to buy course");
+// }
+
+// if (userLoggedIn || loggedInFromEmail){
+//     console.log("User logged in");
+// }
---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,24 @@
+// const month=9;
+// switch (month){
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+//     case 6:
+//         console.log("June");
+//         break;
+//     default:
+//         console.log("July, August, September, October, November, December");
+//         break;
+// }
